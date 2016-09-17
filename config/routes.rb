c2dfd2aae04627196f6de94e3b59128df3b2b395
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2016-09-18 01:27:11 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root 'images#index'
+
   scope protocol: 'https://', constrains: {protocol: 'https'} do
-    root 'images#index'
-
     get "i/:id" => "raw_data#show", as: :raw_datum
-
-    resources :raw_data, only: %i(show)
-
-    resources :images
   end
+
+  resources :raw_data, only: %i(show)
+
+  resources :images
 end
